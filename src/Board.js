c222cc0f72a6100ac6e0a67cb39547936be94e3c
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-06-30 23:16:53 +0530

    Add option to switch between x and o

--- src/Board.js
+++ src/Board.js
@@ -5,16 +5,17 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    console.log(squares);
+    squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      squares: squares
+      squares: squares,
+      xIsNext: !this.state.xIsNext
     });
   };
 
@@ -28,7 +29,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
